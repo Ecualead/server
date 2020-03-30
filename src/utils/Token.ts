eb953e6d019257c6674b1d06c0c214309646946e
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-03-30 03:12:26 -0500

    Adding utility class to generate random tokens
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

---
+++ src/utils/Token.ts
@@ -0,0 +1,10 @@
+/**
+ * @Author: Reinier Millo Sánchez <millo>
+ * @Date:   2020-03-30T02:59:33-05:00
+ * @Email:  reinier.millo88@gmail.com
+ * @Project: ProjectName
+ * @Filename: Token.ts
+ * @Last modified by:   millo
+ * @Last modified time: 2020-03-30T02:59:33-05:00
+ * @Copyright: Copyright 2020 IKOA Business Opportunity
+ */
