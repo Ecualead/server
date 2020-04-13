d05544357c32ef9e44290edf97a41086d7b411cb
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-04-12 21:56:18 -0500

    Update base error handler

--- src/middlewares/ResponseHandler.ts
+++ src/middlewares/ResponseHandler.ts
@@ -4,34 +4,34 @@
  * @Email:  reinier.millo88@gmail.com
  * @Project: IKOABO Core Microservice API
  * @Filename: ResponseHandler.ts
- * @Last modified by:   Reinier Millo Sánchez
- * @Last modified time: 2020-03-24T04:08:49-05:00
+ * @Last modified by:   millo
+ * @Last modified time: 2020-04-12T21:54:40-05:00
  * @Copyright: Copyright 2020 IKOA Business Opportunity
  */
 
- import { Request, Response, NextFunction } from 'express';
- import { HTTP_STATUS } from '../types/status';
+import { Request, Response, NextFunction } from 'express';
+import { HTTP_STATUS } from '../types/status';
 
- export class ResponseHandler {
+export class ResponseHandler {
 
-   /**
-    * Send a success response
-    */
-   public static success(_req: Request, res: Response, _next: NextFunction) {
-     res.status(HTTP_STATUS.HTTP_OK).json(res.locals['response']).end();
-   }
+  /**
+   * Send a success response
+   */
+  public static success(_req: Request, res: Response, _next: NextFunction) {
+    res.status(HTTP_STATUS.HTTP_OK).json(res.locals['response']).end();
+  }
 
-   /**
-    * Send an error response
-    */
-   public static error(err: any, _req: Request, res: Response, _next: NextFunction) {
-     const status = err.status ? err.status : HTTP_STATUS.HTTP_BAD_REQUEST;
-     let error = {
-       error: err.error,
-     }
-     if (err.data) {
-       (<any>error)['data'] = err.data;
-     }
-     res.status(status).json(error).end();
-   }
- }
+  /**
+   * Send an error response
+   */
+  public static error(err: any, _req: Request, res: Response, _next: NextFunction) {
+    const status = err.boStatus ? err.boStatus : HTTP_STATUS.HTTP_BAD_REQUEST;
+    let error = {
+      error: err.boError ? err.boError : HTTP_STATUS.HTTP_INTERNAL_SERVER_ERROR,
+    }
+    if (err.boData) {
+      (<any>error)['data'] = err.boData;
+    }
+    res.status(status).json(error).end();
+  }
+}
