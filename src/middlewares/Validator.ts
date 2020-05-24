b8a69291192216cda4fea9c5a216277b3cfd4f83
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-05-23 22:40:59 -0500

    Fix validation error
    * Handle errors

--- src/middlewares/Validator.ts
+++ src/middlewares/Validator.ts
@@ -14,23 +14,28 @@ import { Request, Response, NextFunction } from 'express';
 import { ERRORS } from '../types/errors';
 
 export class Validators {
+  private static _handleErr(err: any) {
+    let fields: string[] = [];
+    (err.details || []).forEach((value: any) => {
+      if (Array.isArray(value['path']) && value['path'].length > 0) {
+        fields.push(value['path'][value['path'].length - 1])
+      }
+    });
+    return {
+      boError: ERRORS.INVALID_FIELDS,
+      boData: { fields: fields }
+    };
+  }
+
   /**
    * Return middleware to validate request with JOI schema
    */
   public static joi(schema: any, reqField: string = 'body') {
     return (req: Request, _res: Response, next: NextFunction) => {
-      const result = Joi.attempt((<any>req)[reqField], schema, { abortEarly: false });
-      if (result.error) {
-        let fields: string[] = [];
-        result.error.details.forEach((value: any) => {
-          if (Array.isArray(value['path']) && value['path'].length > 0) {
-            fields.push(value['path'][value['path'].length - 1])
-          }
-        });
-        next({
-          boError: ERRORS.INVALID_FIELDS,
-          boData: { fields: fields }
-        });
+      try {
+        Joi.attempt((<any>req)[reqField], schema, { abortEarly: false });
+      } catch (err) {
+        next(Validators._handleErr(err));
         return;
       }
       next();
