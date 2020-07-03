bcffd633699e77a018c0dda01b17e105c77e53ae
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-07-03 14:27:51 -0500

    Update base model
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

--- src/models/base.model.ts
+++ src/models/base.model.ts
@@ -1,19 +1,20 @@
-import { prop } from '@typegoose/typegoose';
-import mongoose from 'mongoose';
+import { prop } from "@typegoose/typegoose";
+import mongoose from "mongoose";
+import { BASE_STATUS } from "../types/status";
 
 export class BaseModel {
-    @prop({ required: true })
-    status!: number;
+  @prop({ required: true, default: BASE_STATUS.BS_ENABLED })
+  status?: number;
 
-    @prop({ type: mongoose.Types.ObjectId, required: true })
-    owner!: string;
+  @prop({ type: mongoose.Types.ObjectId })
+  owner?: string;
 
-    @prop()
-    createdAt?: Date;
+  @prop()
+  createdAt?: Date;
 
-    @prop()
-    updatedAt?: Date;
+  @prop()
+  updatedAt?: Date;
 
-    @prop({ type: mongoose.Types.ObjectId, required: true })
-    modifiedBy!: string;
+  @prop({ type: mongoose.Types.ObjectId })
+  modifiedBy?: string;
 }
