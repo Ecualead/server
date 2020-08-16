5a70ae45e78fcbc9a81806caad1a563660161b70
Reinier Millo Sánchez <millo@ikoabo.com>
2020-08-16 00:24:08 -0500

    Remove unused source code
    Signed-off-by: Reinier Millo Sánchez <millo@ikoabo.com>

--- src/models/geojson.model.ts
+++
@@ -1,9 +0,0 @@
-import { prop, arrayProp } from '@typegoose/typegoose';
-
-export class GeoJSON {
-    @prop({ required: true, enum: ['Point', 'LineString', 'Polygon', 'MultiPoint', 'MultiLineString', 'MultiPolygon'] })
-    type!: string;
-
-    @arrayProp({ items: Number })
-    coordinates!: number[];
-}
